ffff1f893010e1d916d5cf8be6ca332bd75bb6ad
Timilehin Arigbede <harriemannie@gmail.com>
2024-02-21 10:11:33 +0000

    User login functionality with error handling

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -15,16 +15,41 @@ session_set_cookie_params([
 
 session_start();
 
-if (!isset($_SESSION['last_regeneration'])) {
-  regenerate_session_id();
+if (isset($_SESSION['user_id'])) {
+  if (!isset($_SESSION['last_regeneration'])) {
+    regenerate_loggedin_session_id();
+  } else {
+    $interval = 60 * 30;
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+      regenerate_loggedin_session_id();
+    }
+  }
 } else {
-  $interval = 60 * 30;
-  if (time() - $_SESSION["last_regeneration"] >= $interval) {
+  if (!isset($_SESSION['last_regeneration'])) {
     regenerate_session_id();
+  } else {
+    $interval = 60 * 30;
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+      regenerate_session_id();
+    }
   }
 }
 
+
+// Regenerate session ID for logged in user
+function regenerate_loggedin_session_id() {
+  // get user id from session
+  $user_id = $_SESSION["user_id"];
+
+  $newSessionId = session_create_id();
+  $sessionId = $newSessionId . "_" . $user_id;
+  session_id($sessionId);
+
+  $_SESSION["last_generation"] = time();
+}
+
+// Regenerate session ID every 3 minutes for security purpose
 function regenerate_session_id() {
-  session_regenerate_id();
+  session_regenerate_id(true);
     $_SESSION["last_regeneration"] = time();
 }
