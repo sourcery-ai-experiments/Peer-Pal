37287508b40766a0e910e64cd1dd8e0fae9f698d
Timilehin Arigbede <harriemannie@gmail.com>
2024-02-21 14:17:23 +0000

    Styling Signin page style for mobile

--- signup.php
+++ signup.php
@@ -8,23 +8,27 @@ require_once "includes/signup_view.inc.php";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/homepage.css">
+  <link rel="stylesheet" href="/css/signup.css">
   <title>PeerPal | Signup Page</title>
 </head>
-<body>
-  <h3>Signup</h3>
-  
-  <form action="includes/signup.inc.php" method="POST">
-    <?php 
-      signup_inputs();
-    ?>
-    <button>signup</button>
-    <hr>
-    <a href="/login.php">login</a>
-  </form>
+<body id="signup-page">
+  <main class="signup__container">
+    <div class="signup__content">
+      <h3>Welcome to PeerPal</h3>
+      <p>Please enter your details</p>
+      
+      <form action="includes/signup.inc.php" method="POST">
+        <?php 
+          signup_inputs();
+        ?>
+        <button>signup</button>
+        <a href="/login.php">login</a>
+      </form>
 
-  <?php 
-    check_signup_errors();
-  ?>
+      <?php 
+        check_signup_errors();
+      ?>
+    </div>
+  </main>
 </body>
 </html>
