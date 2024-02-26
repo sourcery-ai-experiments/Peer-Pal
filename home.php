cbf57ac0423d1283a23b48b23f959fcc174992b6
JoyRob <you@example.com>
2024-02-26 12:29:33 +0100

    editing

--- home.php
+++ home.php
@@ -4,10 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/css/homepage.css">
+  <link rel="stylesheet" href="./css/footer.css">
+
   <title>PeerPal | HomePage</title>
+  <style>
+    .flex-container{
+  display: flex;
+  background-color: DodgerBlue;
+    }
+  </style>
 </head>
 <body>
- 
   <div class="hero">
     <div class="hero__content">
       <h1 class="hero__title">BuddyConnect: Forge Connections, Foster Growth</h1>
@@ -16,8 +23,44 @@
     </div>
   </div>
 </body>
-<footer >
-<h1> Creating the right connections</h1>  
-<h4>copyright 2024</h4>
+<footer class ="foot" >
+<h2>PEER PAL</h2><br>
+    <div >
+     <div>
+     <p>Lorem ipsum dolor sit amet.</p>
+        <p>  Get the app!    </p>
+     </div>
+    </div><br>
+            <a href="#">Download for iOS</a>
+            <a href="#">Download for Android</a><br>
+     
+        <p class ="navigation">
+            <a href="#">Legal_terms</a>
+            <a href="#">Privacy</a>
+            <a href="#">Terms</a>
+            <a href="#">Policy</a>
+            <a href="#">Property</a>
+            <a href="#">Careers</a>
+            <a href="#">portal</a>
+            <a href="#">Blog</a>
+            <a href="#">FAQ</a>
+            <a href="#">Safety</a>
+            <a href="#">Terms</a>
+        </p>
+        <p class ="navigation">
+            <a href="#">Instagram</a>
+            <a href="#">TikTok</a>
+            <a href="#">YouTube</a>
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Destinations</a>
+            <a href="#">Press</a>
+            <a href="#">Contact</a>
+            <a href="#">Cookies</a>
+            <a href="#">Privacy</a>
+        </p><br>
+        
+            <h3> Creating the right connections</h3>  
+            &copy; 2024 PEER PAL GROUP LLC, All Rights Reserved
 </footer>
 </html>
