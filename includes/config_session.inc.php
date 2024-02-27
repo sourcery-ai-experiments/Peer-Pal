42c6c45d03a227a1c697e69c3ad1b7273a37b6cf
Timilehin Arigbede <harriemannie@gmail.com>
2024-02-27 17:27:16 +0000

    testing

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -7,7 +7,8 @@ ini_set("session.use_strict_mode", 1);
 
 session_set_cookie_params([
   'lifetime' => 1800,
-  'domain' => 'localhost',
+  'domain' => 'heroku.com',
+  // 'domain' => 'localhost',
   'path' => '/',
   'secure' => true,
   'httponly' => true,
