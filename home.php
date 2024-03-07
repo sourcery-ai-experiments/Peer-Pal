5b0696afd7b72675a986609552bc985637f35578
Timilehin Arigbede <harriemannie@gmail.com>
2024-03-07 13:23:49 +0000

    Redesign Navbar and Footer Design

--- home.php
+++ home.php
@@ -3,260 +3,89 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css ">
   <link rel="stylesheet" href="/css/homepage.css">
   <link rel="stylesheet" href="./css/footer.css">
+  <link rel="stylesheet" href="./css/navbar.css">
 
   <title>PeerPal | HomePage</title>
-  <style>
-    .flex-container{
-  display: flex;
-  background-color: DodgerBlue;
-    }
-  </style>
 </head>
 <body>
   <!-- HEADER / NAVBAR -->
-    <style>
-      
-      * {   
-                        margin: 0;
-                        padding: 0;
-                        box-sizing: border-box;
-                    }
-
-
-                header {
-                       
-                        padding: 10px; 
-                        }
-
-
-
-                nav  {
-                        display: flex;
-                       align-items: center;
-                        justify-content: space-between;
-                    }
-
-
-                
-                    a {
-                        
-                        color: #000;
-                        text-decoration: none;
-                        padding: 20px;
-                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                        font-size: 14px; 
-                        position: relative; 
-                    }
-
-                
-                    a::before {
-                            content: '';
-                            position: absolute;
-                            left: 0;
-                            bottom: -2px; 
-                            width: 100%;
-                            height: 2px; 
-                            background-color:purple;
-                            transition: width 0.3s ease; 
-                            opacity: 0; 
-                        }
-                    
-                
-                        a:hover::before {
-                            opacity: 1; 
-                        }
-                        
-                        
-
-                    ul {
-                        
-                            list-style-type: none;
-                            display: flex;
-                        }
-        
-
-                    .log {
-                    height: 150px;
-                    width: auto; 
-                    margin-right: 40px;
-                         }
-             
-           
-
-                    .dop {
-                    margin-left: 120PX;
-                    position: relative;
-                    display: inline-block;
-                         }
-
-                    
-                    .dok {
-                        background-color: purple;
-                        color: #000;
-                        padding: 10px;
-                        border: none;
-                        cursor: pointer;
-                        font-size:medium; 
-                        font-weight:100; 
-                        font-family: 'Arial', sans-serif;
-                        margin-right: 30px;
-                        width: 100px;
-                        border-radius: 20px;
-
-                          }
-
-                .dok:hover{
-                        background-color: #140202;
-                        color: rgb(255, 255, 255);
-                        height: 40px;
-                          }
-                    
-                     .dox {
-                        display: none;
-                        position: absolute;
-                        background-color: #fff;
-                        min-width: 160px;
-                        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                        z-index: 1;
-                    
-                           }
-
-                    
-                    .dox a {
-                        color: #000;
-                        padding: 12px 16px;
-                        text-decoration: none;
-                        display: block;
-
-                        
-                           }
-
-                    
-              .dox a:hover {
-                        background-color: #f0f0f0;
-                        
-                           }
-
-                    
-            .dop:hover .dox{
-                        display: block;
-                        
-                            }
-
-
-    </style>
-
-
-
-</head>
-
-
-
-<body>
-    
-<header>
-
-    <nav>
-
-
-       
-        <nav>
-            
-           <img src="pawon.png" class="log">
-    
-          </nav>
-
-
-        <ul>
-            <li><a href="#">Home</a></li>
-           
-            <li><a href="#">Find Friends</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Notifications</a></li>
-            <li><a href="#">Explore</a></li>
-          
+  <header class="header">
+    <div class="header-content">
+      <a href="/">
+        <img id="logo" src="/images/PeerPals.png" alt="Peer Pal logo">
+      </a>
+      <nav class="nav">
+        <ul class="nav-links">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact us</a>
+          </li>
+          <li>
+            <a href="/login.php">Login</a>
+          </li>
+          <li>
+            <a href="/signup.php">Register</a>
+          </li>
         </ul>
-       
-        <ul>
-          
-
-            <div class="dop">
-                <button class="dok">More</i></button>
-                <div class="dox">
-                 <a href="#">Events</a>
-                  <a href="#">About</a>
-                  <a href="#">Help/Support</a>
-                  <a style="text-align: center;" href="#">Log out</a>
-                </div>
-            </div>
-               
-        
-    </ul>
-    
+        <img src="/images/icons/menu.png" alt="menu">
+    </div>
     </nav>
 
-
-
- </div>
-
-</header>
-
-
-
-<script>
-    function toggleMenu() {
-      var menu = document.getElementById("menu");
-      menu.style.display = menu.style.display === "block" ? "none" : "block";
-    }
-
-  
-    function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.style.display = menu.style.display === "none" ? "block" : "none";
-  }
-</script>
-</body>
-<!-- MAIN -->
-<div class="hero">
-  <div class="hero__content">
-    <h1 class="hero__title">BuddyConnect: Forge Connections, Foster Growth</h1>
-    <p class="hero__subtitle">Discover Your Perfect Study Buddy and Thrive Together</p>
-    <a href="/login.php" class="hero__button">Browse</a>
+  </header>
+  <!-- MAIN -->
+  <div class="hero">
+    <div class="hero__content">
+      <h1 class="hero__title">BuddyConnect: Forge Connections, Foster Growth</h1>
+      <p class="hero__subtitle">Discover Your Perfect Study Buddy and Thrive Together</p>
+      <a href="/login.php" class="hero__button">Browse</a>
+    </div>
   </div>
-</div>
-<footer class ="foot" >
-     
-        <p class ="navigation">
-            <a href="#">Legal_terms</a>
-            <a href="#">Privacy</a>
-            <a href="#">Terms</a>
-            <a href="#">Policy</a>
-            <a href="#">Property</a>
-            <a href="#">Careers</a>
-            <a href="#">portal</a>
-            <a href="#">Blog</a>
-            <a href="#">FAQ</a>
-            <a href="#">Safety</a>
-            <a href="#">Terms</a>
-        </p>
-        <p class ="navigation">
-            <a href="#">Instagram</a>
-            <a href="#">TikTok</a>
-            <a href="#">YouTube</a>
-            <a href="#">Twitter</a>
-            <a href="#">Facebook</a>
-            <a href="#">Destinations</a>
-            <a href="#">Press</a>
-            <a href="#">Contact</a>
-            <a href="#">Cookies</a>
-            <a href="#">Privacy</a>
-        </p><br>
-        
-            <h3> Creating the right connections</h3>  
-            &copy; 2024 PEER PAL GROUP LLC, All Rights Reserved
-</footer>
-</body>
 
+  <footer class="footer">
+    <ul class="footer-links">
+      <li>
+        <a href="#">Terms</a>
+      </li>
+      <li>
+        <a href="#">Contact us</a>
+      </li>
+      <li>
+        <a href="#">Cookies</a>
+      </li>
+      <li>
+        <a href="#">Safety</a>
+      </li>
+      <li>
+        <a href="#">FAQ</a>
+      </li>
+    </ul>
+    <ul class="social-links">
+      <li><a href="#">
+        <img class="social-icon" src="/images/icons/instagram.png" alt="">
+      </a></li>
+      <li><a href="#">
+        <img class="social-icon" src="/images/icons/youtube.png" alt="">
+      </a></li>
+      <li><a href="#">
+        <img class="social-icon" src="/images/icons/facebook.png" alt="">
+      </a></li>
+      <li><a href="#">
+        <img class="social-icon" src="/images/icons/x.png" alt="">
+      </a></li>
+      <li><a href="#">
+        <img class="social-icon" src="/images/icons/linkedin.png" alt="">
+      </a></li>
+      <li><a href="#">
+        <img class="social-icon" src="/images/icons/tiktok.png" alt="">
+      </a></li>
+    </ul>
+    <p>© <?php echo date("Y")  ?> Peerpal | Made with love by PeerPal</p>
+  </footer>
+</body>
 </html>
--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -7,8 +7,7 @@ ini_set("session.use_strict_mode", 1);
 
 session_set_cookie_params([
   'lifetime' => 1800,
-  'domain' => 'heroku.com',
-  // 'domain' => 'localhost',
+  'domain' => 'localhost',
   'path' => '/',
   'secure' => true,
   'httponly' => true,
