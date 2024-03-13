477cc86df74c26e83d45b83590218ee27781ff69
JoyRob <you@example.com>
2024-03-13 16:14:28 +0100

    profile form update

--- components/profile_form.php
+++ components/profile_form.php
@@ -1,32 +1,253 @@
 <h2>Update Personal Details</h2>
 <form action="/includes/update_profile.php" method="POST">
   <label for="first_name">First Name:</label>
-  <input type="text" id="first_name" name="first_name" value="<?php echo $user['first_name']; ?>"><br><br>
+  <input type="text" id="first_name" name="first_name" value="<?php echo $user['first_name']; ?>" placeholder="Your name..">><br><br>
 
   <label for="last_name">Last Name:</label>
   <input type="text" id="last_name" name="last_name" value="<?php echo $user['last_name']; ?>"><br><br>
+
   <label for="date_of_birth">Date of Birth:</label>
   <input type="date" id="date_of_birth" name="date_of_birth" value="<?php echo $user['date_of_birth']; ?>"><br><br>
+
   <label for="nationality">Nationality:</label>
-  <input type="text" id="nationality" name="nationality" value="<?php echo $user['nationality']; ?>"><br><br>
+  <input type="text" id="nationality" name="nationality" value="<?php echo $user['nationality']; ?>">
+  <option value="">-- select one --</option>
+  <option value="afghan">Afghan</option>
+  <option value="albanian">Albanian</option>
+  <option value="algerian">Algerian</option>
+  <option value="american">American</option>
+  <option value="andorran">Andorran</option>
+  <option value="angolan">Angolan</option>
+  <option value="antiguans">Antiguans</option>
+  <option value="argentinean">Argentinean</option>
+  <option value="armenian">Armenian</option>
+  <option value="australian">Australian</option>
+  <option value="austrian">Austrian</option>
+  <option value="azerbaijani">Azerbaijani</option>
+  <option value="bahamian">Bahamian</option>
+  <option value="bahraini">Bahraini</option>
+  <option value="bangladeshi">Bangladeshi</option>
+  <option value="barbadian">Barbadian</option>
+  <option value="barbudans">Barbudans</option>
+  <option value="batswana">Batswana</option>
+  <option value="belarusian">Belarusian</option>
+  <option value="belgian">Belgian</option>
+  <option value="belizean">Belizean</option>
+  <option value="beninese">Beninese</option>
+  <option value="bhutanese">Bhutanese</option>
+  <option value="bolivian">Bolivian</option>
+  <option value="bosnian">Bosnian</option>
+  <option value="brazilian">Brazilian</option>
+  <option value="british">British</option>
+  <option value="bruneian">Bruneian</option>
+  <option value="bulgarian">Bulgarian</option>
+  <option value="burkinabe">Burkinabe</option>
+  <option value="burmese">Burmese</option>
+  <option value="burundian">Burundian</option>
+  <option value="cambodian">Cambodian</option>
+  <option value="cameroonian">Cameroonian</option>
+  <option value="canadian">Canadian</option>
+  <option value="cape verdean">Cape Verdean</option>
+  <option value="central african">Central African</option>
+  <option value="chadian">Chadian</option>
+  <option value="chilean">Chilean</option>
+  <option value="chinese">Chinese</option>
+  <option value="colombian">Colombian</option>
+  <option value="comoran">Comoran</option>
+  <option value="congolese">Congolese</option>
+  <option value="costa rican">Costa Rican</option>
+  <option value="croatian">Croatian</option>
+  <option value="cuban">Cuban</option>
+  <option value="cypriot">Cypriot</option>
+  <option value="czech">Czech</option>
+  <option value="danish">Danish</option>
+  <option value="djibouti">Djibouti</option>
+  <option value="dominican">Dominican</option>
+  <option value="dutch">Dutch</option>
+  <option value="east timorese">East Timorese</option>
+  <option value="ecuadorean">Ecuadorean</option>
+  <option value="egyptian">Egyptian</option>
+  <option value="emirian">Emirian</option>
+  <option value="equatorial guinean">Equatorial Guinean</option>
+  <option value="eritrean">Eritrean</option>
+  <option value="estonian">Estonian</option>
+  <option value="ethiopian">Ethiopian</option>
+  <option value="fijian">Fijian</option>
+  <option value="filipino">Filipino</option>
+  <option value="finnish">Finnish</option>
+  <option value="french">French</option>
+  <option value="gabonese">Gabonese</option>
+  <option value="gambian">Gambian</option>
+  <option value="georgian">Georgian</option>
+  <option value="german">German</option>
+  <option value="ghanaian">Ghanaian</option>
+  <option value="greek">Greek</option>
+  <option value="grenadian">Grenadian</option>
+  <option value="guatemalan">Guatemalan</option>
+  <option value="guinea-bissauan">Guinea-Bissauan</option>
+  <option value="guinean">Guinean</option>
+  <option value="guyanese">Guyanese</option>
+  <option value="haitian">Haitian</option>
+  <option value="herzegovinian">Herzegovinian</option>
+  <option value="honduran">Honduran</option>
+  <option value="hungarian">Hungarian</option>
+  <option value="icelander">Icelander</option>
+  <option value="indian">Indian</option>
+  <option value="indonesian">Indonesian</option>
+  <option value="iranian">Iranian</option>
+  <option value="iraqi">Iraqi</option>
+  <option value="irish">Irish</option>
+  <option value="israeli">Israeli</option>
+  <option value="italian">Italian</option>
+  <option value="ivorian">Ivorian</option>
+  <option value="jamaican">Jamaican</option>
+  <option value="japanese">Japanese</option>
+  <option value="jordanian">Jordanian</option>
+  <option value="kazakhstani">Kazakhstani</option>
+  <option value="kenyan">Kenyan</option>
+  <option value="kittian and nevisian">Kittian and Nevisian</option>
+  <option value="kuwaiti">Kuwaiti</option>
+  <option value="kyrgyz">Kyrgyz</option>
+  <option value="laotian">Laotian</option>
+  <option value="latvian">Latvian</option>
+  <option value="lebanese">Lebanese</option>
+  <option value="liberian">Liberian</option>
+  <option value="libyan">Libyan</option>
+  <option value="liechtensteiner">Liechtensteiner</option>
+  <option value="lithuanian">Lithuanian</option>
+  <option value="luxembourger">Luxembourger</option>
+  <option value="macedonian">Macedonian</option>
+  <option value="malagasy">Malagasy</option>
+  <option value="malawian">Malawian</option>
+  <option value="malaysian">Malaysian</option>
+  <option value="maldivan">Maldivan</option>
+  <option value="malian">Malian</option>
+  <option value="maltese">Maltese</option>
+  <option value="marshallese">Marshallese</option>
+  <option value="mauritanian">Mauritanian</option>
+  <option value="mauritian">Mauritian</option>
+  <option value="mexican">Mexican</option>
+  <option value="micronesian">Micronesian</option>
+  <option value="moldovan">Moldovan</option>
+  <option value="monacan">Monacan</option>
+  <option value="mongolian">Mongolian</option>
+  <option value="moroccan">Moroccan</option>
+  <option value="mosotho">Mosotho</option>
+  <option value="motswana">Motswana</option>
+  <option value="mozambican">Mozambican</option>
+  <option value="namibian">Namibian</option>
+  <option value="nauruan">Nauruan</option>
+  <option value="nepalese">Nepalese</option>
+  <option value="new zealander">New Zealander</option>
+  <option value="ni-vanuatu">Ni-Vanuatu</option>
+  <option value="nicaraguan">Nicaraguan</option>
+  <option value="nigerien">Nigerien</option>
+  <option value="nigerian">Nigerian</option>
+  <option value="north korean">North Korean</option>
+  <option value="northern irish">Northern Irish</option>
+  <option value="norwegian">Norwegian</option>
+  <option value="omani">Omani</option>
+  <option value="pakistani">Pakistani</option>
+  <option value="palauan">Palauan</option>
+  <option value="panamanian">Panamanian</option>
+  <option value="papua new guinean">Papua New Guinean</option>
+  <option value="paraguayan">Paraguayan</option>
+  <option value="peruvian">Peruvian</option>
+  <option value="polish">Polish</option>
+  <option value="portuguese">Portuguese</option>
+  <option value="qatari">Qatari</option>
+  <option value="romanian">Romanian</option>
+  <option value="russian">Russian</option>
+  <option value="rwandan">Rwandan</option>
+  <option value="saint lucian">Saint Lucian</option>
+  <option value="salvadoran">Salvadoran</option>
+  <option value="samoan">Samoan</option>
+  <option value="san marinese">San Marinese</option>
+  <option value="sao tomean">Sao Tomean</option>
+  <option value="saudi">Saudi</option>
+  <option value="scottish">Scottish</option>
+  <option value="senegalese">Senegalese</option>
+  <option value="serbian">Serbian</option>
+  <option value="seychellois">Seychellois</option>
+  <option value="sierra leonean">Sierra Leonean</option>
+  <option value="singaporean">Singaporean</option>
+  <option value="slovakian">Slovakian</option>
+  <option value="slovenian">Slovenian</option>
+  <option value="solomon islander">Solomon Islander</option>
+  <option value="somali">Somali</option>
+  <option value="south african">South African</option>
+  <option value="south korean">South Korean</option>
+  <option value="spanish">Spanish</option>
+  <option value="sri lankan">Sri Lankan</option>
+  <option value="sudanese">Sudanese</option>
+  <option value="surinamer">Surinamer</option>
+  <option value="swazi">Swazi</option>
+  <option value="swedish">Swedish</option>
+  <option value="swiss">Swiss</option>
+  <option value="syrian">Syrian</option>
+  <option value="taiwanese">Taiwanese</option>
+  <option value="tajik">Tajik</option>
+  <option value="tanzanian">Tanzanian</option>
+  <option value="thai">Thai</option>
+  <option value="togolese">Togolese</option>
+  <option value="tongan">Tongan</option>
+  <option value="trinidadian or tobagonian">Trinidadian or Tobagonian</option>
+  <option value="tunisian">Tunisian</option>
+  <option value="turkish">Turkish</option>
+  <option value="tuvaluan">Tuvaluan</option>
+  <option value="ugandan">Ugandan</option>
+  <option value="ukrainian">Ukrainian</option>
+  <option value="uruguayan">Uruguayan</option>
+  <option value="uzbekistani">Uzbekistani</option>
+  <option value="venezuelan">Venezuelan</option>
+  <option value="vietnamese">Vietnamese</option>
+  <option value="welsh">Welsh</option>
+  <option value="yemenite">Yemenite</option>
+  <option value="zambian">Zambian</option>
+  <option value="zimbabwean">Zimbabwean</option><br><br>
+
   <label for="gender">Gender:</label>
   <select name="gender" id="gender">
-    <option value="female" <?php if(isset($user['gender']) && $user['gender'] === 'female') echo 'selected'; ?>>Female</option>
-    <option value="male" <?php if(isset($user['gender']) && $user['gender'] === 'male') echo 'selected'; ?>>Male</option>
+    <option value="man" <?php if (isset($user['gender']) && $user['gender'] === 'man') echo 'selected'; ?>>man</option>
+    <option value="woman" <?php if (isset($user['gender']) && $user['gender'] === 'woman') echo 'selected'; ?>>woman</option>
+    <option value="transgender man" <?php if (isset($user['gender']) && $user['gender'] === 'transgender man') echo 'selected'; ?>>transgender man</option>
+    <option value="transgender woman" <?php if (isset($user['gender']) && $user['gender'] === 'transgender woman') echo 'selected'; ?>>transgender woman</option>
+    <option value="Non-binary" <?php if (isset($user['gender']) && $user['gender'] === 'non-binary') echo 'selected'; ?>>Non-binary</option>
+    <option value="Agender" <?php if (isset($user['gender']) && $user['gender'] === 'Agender') echo 'selected'; ?>>Agender</option>
+    <option value="gender not listed" <?php if (isset($user['gender']) && $user['gender'] === 'gender not listed') echo 'selected'; ?>>gender not listed</option>
+    <option value="prefer not to say" <?php if (isset($user['gender']) && $user['gender'] === 'prefer not to say') echo 'selected'; ?>>prefer not to say</option>
   </select>
-  
+
   <label for="faculty">Faculty:</label>
-  <input type="text" id="faculty" name="faculty" value="<?php echo $user['faculty']; ?>"><br><br>
-  <label for="study_mode">Study Mode (Full or Part-time):</label>
-  <input type="text" id="study_mode" name="study_mode" value="<?php echo $user['study_mode']; ?>"><br><br>
+  <input type="text" id="faculty" name="faculty" value="<?php echo $user['faculty']; ?>">
+  <option value="Department of accounting, finance and education">Department of accounting, finance and education</option>
+  <option value="Department of communication, marketing and media">Department of communication, marketing and media</option>
+  <option value="Department of information management">Department of information management</option>
+  <option value="Department of law">Department of law</option>
+  <option value="school of applied social sciences ">school of Applied social sciences</option>
+  <option value="school of Health sciences ">school of Health sciences</option>
+  <option value="school of Nursing and Midwifery">school of Nursing and Midwifery</option>
+  <option value="school of Pharmarcy andLife sciences">school of Pharmacy and Life sciences</option>
+  <option value="school of Computing">school of Computing</option>
+  <option value="school of Engineering">school of Engineering</option>
+  <option value="Grey's school of Art">Grey's school of Art</option>
+  <option value="Scott Sutherland School of Architecture & Built Environment">Scott Sutherland School of Architecture & Built Environment</option>
+  <br><br>
+
+  <label for="study_mode">Study Mode (Full or school of):</label>
+  <input type="text" id="study_mode" name="study_mode" value="<?php echo $user['study_mode']; ?>">
+  <option value="Full time">Full-time</option>
+  <option value="Part-time">Part-time</option>
+  <br><br>
+
   <label for="photo">Photo:</label>
   <input type="text" id="photo" name="photo" value="<?php echo $user['photo']; ?>"><br><br>
   <label for="program_type">Degree Level:</label>
   <input type="text" id="program_type" name="program_type" value="<?php echo $user['program_type']; ?>"><br><br>
   <label for="student_type">New Student or Existing Student:</label>
   <select name="student_type" id="student_type">
-    <option value="new student" <?php if(isset($user['student_type']) && $user['student_type'] === 'new student') echo 'selected'; ?>>New Student</option>
-    <option value="existing student" <?php if(isset($user['student_type']) && $user['student_type'] === 'existing student') echo 'selected'; ?>>Existing student</option>
+    <option value="new student" <?php if (isset($user['student_type']) && $user['student_type'] === 'new student') echo 'selected'; ?>>New Student</option>
+    <option value="existing student" <?php if (isset($user['student_type']) && $user['student_type'] === 'existing student') echo 'selected'; ?>>Existing student</option>
   </select>
   <button type="submit">Update Profile</button>
 </form>
