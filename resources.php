3da3e6613cfc9f2eb14719417d95f4d77c7ab0a2
EbubeRebecca <157708013+EbubeRebecca@users.noreply.github.com>
2024-03-30 23:12:59 +0000

    feat: complete resources page

---
+++ resources.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="./css/navbar.css">
+      <link rel="stylesheet" href="./css/footer.css">
+      <link rel="stylesheet" href="./css/resources.css">
+      <title>PeerPal | Resources</title>
+</head>
+<body>
+      <!-- HEADER / NAVBAR -->
+      <?php include("./components/navbar.php") ?>
+      <div id="banner">
+            <img src="./images/resources/resources-banner.jpg" />
+            <div class="banner-container">
+                  <div class="banner-desc">
+                        <h1>Resources</h1>
+                  </div>
+                  <div class="banner-desc">
+                        <p>Resources, guides and support, tailored to you</p>
+                  </div>
+            </div>
+      </div>
+      <div id="resources-card-wrapper">
+            <div id="container">
+                  <div class="resource-card">
+                        <div>
+                              <img src="./images/resources/finance.jpg" alt=""/>
+                        </div>
+                        <div>
+                              <h3>Finance & Tax</h3>
+                              <ul class="list">
+                                    <li><a href="https://revolut.com/referral/stanle8fip!FEB2-23-AR-H1">Open a bank account</a></li>
+                                    <li><a href="https://www.gov.uk/apply-for-council-tax-discount">Apply for Council tax exemption</a></li>
+                              </ul>
+                        </div>
+                  </div>
+                  <div class="resource-card">
+                        <div>
+                              <img src="./images/resources/transportation.jpg" alt=""/>
+                        </div>
+                        <div>
+                              <h3>Transportation</h3>
+                              <ul class="list">
+                                    <li><a href="https://www.gov.uk/apply-first-provisional-driving-licence">Apply for provisional Driver's License</a></li>
+                                    <li><a href="https://www.firstbus.co.uk/">Get the First Bus App</a></li>
+                                    <li><a href="https://www.stagecoachbus.com/">Travel Longer Distances using stage Coach</a></li>
+                              </ul>
+                        </div>
+                  </div>
+                  <div class="resource-card">
+                        <div>
+                              <img src="./images/resources/health.jpg" alt=""/>
+                        </div>
+                        <div>
+                              <h3>Health Care</h3>
+                              <ul class="list">
+                                    <li><a href="https://www.nhs.uk/service-search/find-a-gp/">Register with a GP</a></li>
+                              </ul>
+                        </div>
+                  </div>
+                  <div class="resource-card">
+                        <div>
+                              <img src="./images/resources/jobs.jpg" alt=""/>
+                        </div>
+                        <div>
+                              <h3>Jobs & Requirements</h3>
+                              <ul class="list">
+                                    <li><a href="https://www.gov.uk/apply-national-insurance-number">Apply for NI (National Insurance Number)</a></li>
+                                    <li><a href="https://www.gov.uk/prove-right-to-work">Get a shared code for your right to work in the UK</a></li>
+                              </ul>
+                              <div id="sub-links">
+                                    <a href="https://www.totaljobs.com/">Total Jobs</a>
+                                    <a href="https://uk.indeed.com/">Indeed</a>
+                                    <a href="https://www.studentjob.co.uk">Student Jobs</a>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="resource-card">
+                        <div>
+                              <img src="./images/resources/academics.jpg" alt=""/>
+                        </div>
+                        <div>
+                              <h3>Academics</h3>
+                              <ul class="list">
+                                    <li><a href="https://library.rgu.ac.uk/past-exam-papers">Practice past exam questions</a></li>
+                                    <li><a href="https://library.rgu.ac.uk/education">Complete your assignments, courseworks and term papers with the materials from the school library</a></li>
+                                    <li><a href="https://www.rgu.ac.uk/your-attendance">Mark your attendance using RGU's Attendr</a></li>
+                                    <li><a href="https://www.rgu.ac.uk/myrgu">Everything is seamless with the RGU app</a></li>
+                              </ul>
+                        </div>
+                  </div>
+            </div>
+      </div>
+      <!-- FOOTER -->
+      <?php include("./components/footer.php") ?>
+</body>
+</html>
